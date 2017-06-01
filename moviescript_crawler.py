9ffe72e991d6652675a714849278af462e0d7a8c
drwiner <drwiner131@gmail.com>
2017-06-01 13:28:10 -0600

    moviescript crawler revise

--- moviescript_crawler.py
+++ moviescript_crawler.py
@@ -4,7 +4,9 @@
 # import os
 from os import listdir
 from os.path import isfile, join
-path = 'D:\\Documents\\NLP corpora\\imsdb_scenes_sep_2012\\imsdb_scenes_clean'
+# path = 'D:\\Documents\\NLP corpora\\imsdb_scenes_sep_2012\\imsdb_scenes_clean'
+path = 'D:\\Documents\\python\\screenpy\\imsdb_raw_nov_2015\\'
+from screenpile import *
 
 def joinMovies(movie_paths, combo_name):
 	print('joining movies')
@@ -30,3 +32,37 @@ if __name__ == '__main__':
 	movie_paths = [join(path,f) for f in movie_cats if not isfile(join(path,f))]
 	combo_name = 'movie_combo.txt'
 	joinMovies(movie_paths, combo_name)
+
+	RELOAD = 0
+	screenplay = 'indianajonesandtheraidersofthelostark.txt'
+	if RELOAD:
+		heads = []
+		with open(screenplay, 'r') as fn:
+			play = fn.read()
+			for result, s, t in HEADINGS.scanString(play):
+				indent = result[0]['indent']
+
+				# if indent is a weird screenplay number at random spacing...
+				# don't extract this for now
+				if indent < 13:
+					heading_type = 'in_line'
+				elif indent > 50:
+					heading_type = 'transition'
+				elif indent < 18:
+					heading_type = 'heading'
+				else:
+					heading_type = 'speaker'
+
+				heads.append(Heading(heading_type, result[0], s, t, indent))
+
+		heads.sort(key=lambda y: y.start, reverse=False)
+		pickle.dump(heads, open('pickle_heads.pkl', 'wb'))
+	else:
+		play = open(screenplay, 'r').read()
+		heads = pickle.load(open('pickle_heads.pkl', 'rb'))
+
+	segments = screenpile_algorithm(heads, play)
+	hsegs = separate_into_segs(segments)
+	print('ok')
+	with open('indianajonesandtheraidersofthelostark.json', 'w') as fp:
+		json.dump([seg_to_json(seg, i) for i, seg in enumerate(hsegs)], fp, indent=4)
