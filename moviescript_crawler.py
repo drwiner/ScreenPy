20d83a57b50c36f573901854dcec469be30a6fbc
DW <David@Beatrices-MacBook-Pro.local>
2017-06-03 16:24:51 -0400

    crawler

--- moviescript_crawler.py
+++ moviescript_crawler.py
@@ -8,30 +8,9 @@ from os.path import isfile, join
 path = 'D:\\Documents\\python\\screenpy\\imsdb_raw_nov_2015\\'
 from screenpile import *
 
-def joinMovies(movie_paths, combo_name):
-	print('joining movies')
-	num_movies = 0
-	with open(combo_name, 'w') as cn:
-		for mp in movie_paths:
-			# get all file contents
-			movies = [f for f in listdir(mp) if isfile(join(mp, f))]
-			for movie in movies:
-				num_movies += 1
-				# print('reading ' + movie)
-				with open(join(mp,movie)) as mn:
-					for line in mn:
-						if line == '\n':
-							continue
-						cn.write(line)
-					cn.write('\n')
-	print(num_movies)
-
-
 if __name__ == '__main__':
 	movie_cats = listdir(path)
 	movie_paths = [join(path,f) for f in movie_cats if not isfile(join(path,f))]
-	combo_name = 'movie_combo.txt'
-	joinMovies(movie_paths, combo_name)
 
 	RELOAD = 0
 	screenplay = 'indianajonesandtheraidersofthelostark.txt'
