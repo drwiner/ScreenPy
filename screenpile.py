6cbea63b48fd720a8073f6c8a2b927aa06e3f03b
drwiner <drwiner131@gmail.com>
2017-08-03 16:31:23 -0600

    new parsing strategy

--- screenpile.py
+++ screenpile.py
@@ -192,10 +192,170 @@ def seg_to_json(seg, i):
 	# return None
 
 
+# def tokenize_text(text):
+	# word_tokens = [nltk.word_tokenize(text)]
+
+import re
+import string
+import nltk
+
+# lines are at most 57 characters
+RANGE_LEFT = range(18)
+RANGE_MID = range(18, 50)
+RANGE_RIGHT = range(50,58)
+
+def clean_tokenization(token_list):
+	pattern = re.compile('[{}]'.format(re.escape(string.punctuation)))
+	filtered_tokens = filter(None, [pattern.sub('', token) for token in token_list])
+	return filtered_tokens
+
+def is_upper(text):
+	# tokenize
+	tokens = nltk.word_tokenize(text)
+
+	# prune special characters
+	filtered_tokens = clean_tokenization(tokens)
+
+	# return False when non caps found
+	for token in filtered_tokens:
+		if not token.isupper():
+			return False
+	return True
+
+
+from collections import namedtuple
+
+seg = namedtuple('seg', 'head_type head_text text')
+
+
+def segmentize(tuple_lines):
+	segments = []
+	previous = None
+	current_seg = None
+	for (typ, _, this_text) in tuple_lines:
+		if typ in {'heading', 'speaker/title', 'transition'}:
+			if current_seg is not None:
+				segments.append(current_seg)
+			current_seg = seg(typ, this_text, [])
+			previous = typ
+		elif typ in {'direction', 'dialogue'}:
+			# change to new segment when we go direction -> dialogue
+			if typ is 'direction' and previous is 'dialogue':
+				if current_seg is not None:
+					segments.append(current_seg)
+				current_seg = seg('heading', '', [])
+			current_seg.text.append(this_text)
+			previous = typ
+
+	segments.append(current_seg)
+	return segments
+
+
+def master_segmentize(segs):
+	master_segs = []
+	master_seg = []
+	for (head_type, head_text, text_list) in segs:
+		# here we'll use pyparsing to extract
+		fleshy_text = '\n'.join(text_list)
+
+		if head_type is 'heading':
+			if head_text is not '':
+				try:
+					parsed_heading = alpha.parseString(head_text)
+				except ValueError('could not parse heading: {}'.format(head_text)):
+					raise
+			else:
+				parsed_heading = {} # empty dict
+
+			if 'terior' in parsed_heading.keys():
+				# dis is a master
+				if len(master_seg) > 0:
+					master_segs.append(master_seg)
+				master_seg = [seg(head_type, parsed_heading, fleshy_text)]
+			else:
+				master_seg.append(seg(head_type, parsed_heading, fleshy_text))
+
+		elif head_type in {'speaker/title', 'transition'}:
+			master_seg.append(seg(head_type, head_text, fleshy_text))
+
+	if len(master_seg) > 0:
+		master_segs.append(master_seg)
+
+	return master_segs
+
+
+
+def assemble_lines(text):
+	established_dialogue_indent = None
+	# established_speaker_indent = None
+	indent_tuples = []
+	start_collecting = False
+	for i, tl in enumerate(text_lines):
+		try:
+			leading_indent = spaces.parseString(tl)['indent']
+		except:
+			leading_indent = 0
+
+		relevant_text = tl[leading_indent:]
+		is_caps = is_upper(relevant_text)
+		# filtered_tokens = clean_tokenization(token_list)
+
+		if not start_collecting:
+			if relevant_text in {'FADE IN:', 'FADE IN'}:
+				start_collecting = True
+
+			if 'INT. ' in relevant_text or 'EXT. ' in relevant_text:
+				start_collecting = True
+
+		if not start_collecting:
+			continue
+
+		if leading_indent in RANGE_LEFT:
+			# figure out if it's all caps + digits
+			if is_caps:
+				indent_tuples.append(('heading', leading_indent, relevant_text))
+			else:
+				indent_tuples.append(('direction', leading_indent, relevant_text))
+
+		elif leading_indent in RANGE_MID:
+			if is_caps:
+				if established_dialogue_indent is not None:
+					if leading_indent in range(established_dialogue_indent-5, established_dialogue_indent+5):
+						# then it's just shouting...
+						indent_tuples.append(('dialogue', leading_indent, relevant_text))
+						continue
+				indent_tuples.append(('speaker/title', leading_indent, relevant_text))
+			else:
+				indent_tuples.append(('dialogue', leading_indent, relevant_text))
+				if established_dialogue_indent is None and i > 100:
+					established_dialogue_indent = leading_indent
+		else:
+			if is_caps:
+				indent_tuples.append(('transition', leading_indent, relevant_text))
+			else:
+				raise ValueError('what the fuck is this: {}\n on line: {}'.format(relevant_text, str(i)))
+	return indent_tuples
+
 if __name__ == '__main__':
 
-	RELOAD = 1
+
+
+	# from nltk.stem import WordNetLemmatizer
+	# wordnet_lemmatizer = WordNetLemmatizer()
+
+	RELOAD = 0
+	DO = 0
 	screenplay = 'indianajonesandtheraidersofthelostark.txt'
+	with open(screenplay, 'r') as fn:
+		play = fn.read()
+
+	text_lines = play.split('\n\n')
+	line_tups = assemble_lines(text_lines)
+	segs = segmentize(line_tups)
+	masta = master_segmentize(segs)
+	print('here')
+
+
 	if RELOAD:
 		heads = []
 		with open(screenplay, 'r') as fn:
@@ -222,13 +382,11 @@ if __name__ == '__main__':
 		play = open(screenplay, 'r').read()
 		heads = pickle.load(open('pickle_heads.pkl', 'rb'))
 
-	segments = screenpile_algorithm(heads, play)
-	hsegs = separate_into_segs(segments)
-	print('ok')
-	with open('indianajonesandtheraidersofthelostark.json', 'w') as fp:
-		json.dump([seg_to_json(seg, i) for i, seg in enumerate(hsegs)], fp, indent=4)
-	# with open('segments.txt', 'w') as seg_file:
-	# 	for seg in segments:
-	# 		seg_file.write(json)
+	if DO:
+		segments = screenpile_algorithm(heads, play)
+		hsegs = separate_into_segs(segments)
+		print('ok')
+		with open('indianajonesandtheraidersofthelostark.json', 'w') as fp:
+			json.dump([seg_to_json(seg, i) for i, seg in enumerate(hsegs)], fp, indent=4)
 
 
